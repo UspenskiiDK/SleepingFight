9763e09beb9071d4345583d17afb293694216596
Uspenskii <uspenskiidk@gmail.com>
2022-12-08 16:18:33 +0400

    Добавил класс DbContext для работы с бд, создал бд с таблицами User и Game c помощью SQL Server Management Studio, подключил бд с помощью строки подключения в appsetings json, в startup добавил подключение в методе configureServices

---
+++ DAL/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DAL
+{
+    public class AppDbContext : DbContext
+    {
+        //конструктор (для работы в MVC - startup)
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options) { }
+
+        //добавим сущность User
+        public DbSet<User> User { get; set; }
+
+    }
+}
--- SleepingFight/Startup.cs
+++ SleepingFight/Startup.cs
@@ -1,6 +1,8 @@
+using DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,12 @@ namespace SleepingFight
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            //добавим возможность работы с DbContext и подключимся к бд
+            var connection = Configuration.GetConnectionString(name: "DefaultConnection");
+            services.AddDbContext<AppDbContext>(optionsAction: options =>
+                 options.UseSqlServer(connection));
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
